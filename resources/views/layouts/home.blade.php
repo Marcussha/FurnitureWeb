5f4e49654673037ffa574f7d5f7c468e261efdcc
PresidentThong <thongha1204@gmail.com>
2022-12-11 12:51:05 +0700

    update login

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title> Admin</title>
+    <title> Login</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{asset('admins/vendors/mdi/css/materialdesignicons.min.css')}}">
     <link rel="stylesheet" href="{{asset('admins/vendors/css/vendor.bundle.base.css')}}">
